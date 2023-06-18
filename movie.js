f44389d83d55d22bbe04bdbe5bb526df909e3688
Alecio L. Medeiros <Alexdesaran@outlook.com>
2023-06-18 11:44:43 -0300

    Adequações Visuais

--- movie.js
+++ movie.js
@@ -1,153 +1,84 @@
 var main = document.querySelectorAll('.main .centro')[0];
 
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&languague=en-US&page=1')
-.then(response=> response.json())
- .then(data=>{
-    data.results.map((i,j)=>{
-        
-        if(j > 14)
-        
-            return;
-        
-            if(j==0){
-                main.innerHTML+=`
-                <h2>Bem Vindo SR. Gado</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }else{
+  .then(response => response.json())
+  .then(data => {
+    data.results.map((i, j) => {
 
-                main.innerHTML+=`
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }
-        
-        
+      if (j > 10)
+        return;
 
-    })
-})
+      if (j != 0) {
+        main.innerHTML += `
+          <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200${i.poster_path}" /></div>
+        `;
+      }
+
+    });
+  });
 
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&languague=en-US&page=2')
-.then(response=> response.json())
- .then(data=>{
-    data.results.map((i,j)=>{
-        
-        if(j > 15)
-            return;
-        if(j == 0){
+  .then(response => response.json())
+  .then(data => {
+    data.results.map((i, j) => {
 
-            main.innerHTML+=`
-                <h2>Assista Aos Melhores Filmes</h2>
-                <div class="banner-principal" style="margin-top:20px;background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
-            `;
-        }else{
-            if(j==1){
-                main.innerHTML+=`
-                <h2>Assista Agora</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }else{
+      if (j > 10)
+        return;
 
-                main.innerHTML+=`
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }
-        
-        }
+      else {
+        main.innerHTML += `
+          <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200${i.poster_path}" /></div>
+        `;
+      }
 
-    })
-})
+    });
+  });
+
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&languague=en-US&page=3')
-.then(response=> response.json())
- .then(data=>{
-    data.results.map((i,j)=>{
-        
-        if(j > 15)
-            return;
-        if(j == 0){
+  .then(response => response.json())
+  .then(data => {
+    data.results.map((i, j) => {
 
-            main.innerHTML+=`
-            <h2>Em Cartaz</h2>
-                 <div class="banner-principal" style="margin-top:20px;background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
-            `;
-        }else{
-            if(j==1){
-                main.innerHTML+=`
-                <h2>Para Assistir Agora</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }else{
+      if (j > 10)
+        return;
+      else {
+        main.innerHTML += `
+          <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200${i.poster_path}" /></div>
+        `;
+      }
 
-                main.innerHTML+=`
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }
-        
-        }
+    });
+  });
 
-    })
-})
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&languague=en-US&page=4')
-.then(response=> response.json())
- .then(data=>{
-    data.results.map((i,j)=>{
-        
-        if(j > 15)
-            return;
-        if(j == 0){
+  .then(response => response.json())
+  .then(data => {
+    data.results.map((i, j) => {
 
-            main.innerHTML+=`
-            <h2>Animações</h2>
-                 <div class="banner-principal" style="margin-top:20px;background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
-            `;
-        }else{
-            if(j==1){
-                main.innerHTML+=`
-                <h2>Ação</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }else{
+      if (j > 15)
+        return;
+       else {
+        main.innerHTML += `
+          <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200${i.poster_path}" /></div>
+        `;
+      }
 
-                main.innerHTML+=`
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }
-        
-        }
+    });
+  });
 
-    })
-})
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&languague=en-US&page=5')
-.then(response=> response.json())
- .then(data=>{
-    data.results.map((i,j)=>{
-        
-        if(j > 15)
-            return;
-        if(j == 0){
+  .then(response => response.json())
+  .then(data => {
+    data.results.map((i, j) => {
 
-            main.innerHTML+=`
-            <h2>Para Animar</h2>
-                 <div class="banner-principal" style="margin-top:20px;background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
-            `;
-        }else{
-            if(j==1){
-                main.innerHTML+=`
-                <h2>Suspense</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }else{
+      if (j > 15)
+        return;
 
-                main.innerHTML+=`
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200`+i.poster_path+`" /></div>
-                `;
-            }
-        
-        }
+      else {
+        main.innerHTML += `
+          <div class="card-movie"><img src="https://image.tmdb.org/t/p/w200${i.poster_path}" /></div>
+        `;
+      }
 
-    })
-})
+    });
+  });
